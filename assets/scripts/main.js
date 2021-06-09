983acdf0c743c136f0ac17c14fe13fb99d429e8b
Kim Zuniga <kim.eli.zuniga@gmail.com>
2021-06-09 11:16:09 -0400

    Add sitemap.xml

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -15,6 +15,7 @@ function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("slides");
   let dots = document.getElementsByClassName("dot");
+
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
@@ -25,4 +26,6 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " activeDot";
+  
 }
+
